e03e0a6d0175d2a9fdfd4f68655221b73064bee4
Jay Son <sweetandsourkiss@gmail.com>
2024-03-18 01:51:19 +0900

    Finish thought setting

--- main.js
+++ main.js
@@ -1,8 +1,26 @@
+const body = document.body;
+
 const github = document.querySelector(".contact_github span");
 const instagram = document.querySelector(".contact_instagram span");
-const naver = document.querySelector(".contact_naver");
+const naver = document.querySelector(".contact_naver span");
+const google = document.querySelector(".contact_google span");
 
-const init = () => {
+const collab_link = document.querySelector(".thought_content-collab");
+const customer_link = document.querySelector(".thought_content-customer");
+
+const background = document.querySelector(".background");
+const collab = document.querySelector(".collaboration");
+const customer = document.querySelector(".customer-centered");
+const close_button = document.querySelectorAll(".close_button");
+
+const setTitle = () => {
+  github.setAttribute("title", "Github 보러가기");
+  instagram.setAttribute("title", "Instagram 보러가기");
+  naver.setAttribute("title", "네이버 이메일 쓰러가기");
+  google.setAttribute("title", "구글 이메일 쓰러가기");
+};
+
+const setPlatformEvent = () => {
   github.addEventListener("click", (__) => {
     window.open("https://github.com/sweetandsourkiss", "_blank");
   });
@@ -18,9 +36,47 @@ const init = () => {
     );
   });
 
-  github.setAttribute("title", "Github 보러가기");
-  instagram.setAttribute("title", "Instagram 보러가기");
-  naver.setAttribute("title", "이메일 쓰러가기");
+  google.addEventListener("click", () => {
+    window.open(
+      "https://mail.google.com/mail/?view=cm&fs=1&to=mooul0529@gmail.com",
+      "_blank"
+    );
+  });
+};
+
+const initialSetting = () => {
+  body.style.overflow = "auto";
+  background.style.display = "none";
+  collab.style.display = "none";
+  customer.style.display = "none";
+};
+
+const setThoughtEvent = () => {
+  close_button.forEach((button) => {
+    button.addEventListener("click", () => {
+      initialSetting();
+    });
+  });
+
+  collab_link.addEventListener("click", () => {
+    body.style.overflow = "hidden";
+    background.style.display = "flex";
+    collab.style.display = "block";
+  });
+
+  customer_link.addEventListener("click", () => {
+    body.style.overflow = "hidden";
+    background.style.display = "flex";
+    customer.style.display = "block";
+  });
+};
+
+const init = () => {
+  console.log(body);
+  initialSetting();
+  setPlatformEvent();
+  setThoughtEvent();
+  setTitle();
 };
 
 init();
